6010451a8e436fd89629b6508a8d1676afef0adc
hanjie <jiehan330@gmail.com>
2023-08-16 20:29:32 +0800

    Get the Ethereum accounts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,32 @@ import {
   heading,
   text,
 } from '@metamask/snaps-ui';
+import { assert } from '@metamask/utils';
 
+/**
+ * Get the Ethereum accounts that the snap has access to using the `ethereum`
+ * global. This is essentially the same as the `window.ethereum` global, but
+ * does not have access to all methods.
+ *
+ * If the user hasn't given the snap access to any accounts yet, this JSON-RPC
+ * method will show a prompt to the user, asking them to select the accounts to
+ * give the snap access to.
+ *
+ * Note that using the `ethereum` global requires the
+ * `endowment:ethereum-provider` permission.
+ *
+ * @returns The selected accounts as an array of hexadecimal strings.
+ * @throws If the user rejects the prompt.
+ * @see https://docs.metamask.io/snaps/reference/permissions/#endowmentethereum-provider
+ */
+async function getAccounts() {
+  const accounts = await ethereum.request<string[]>({
+    method: 'eth_requestAccounts',
+  });
+  assert(accounts, 'Ethereum provider did not return accounts.');
+
+  return accounts as string[];
+}
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -16,12 +41,16 @@ import {
  * @returns The result of `snap_dialog`.
  * @throws If the request method is not valid for this snap.
  */
-export const onRpcRequest: OnRpcRequestHandler = ({ request }) => {
-  const { address, chainId } = request.params;
+export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+  const { chainId } = request.params;
   console.log(request.method);
   switch (request.method) {
     case 'token_detection':
-      return getData(address, chainId).then(async (res) => {
+      return getAccounts().then(async (accounts) => {
+        if (!accounts.length) {
+          throw new Error('Get the Ethereum accounts Fail.');
+        }
+        const res = await getData(accounts[0], chainId);
         const panelArr: any = [];
         if (res.code === 1) {
           res.result.forEach((item: any) => {
@@ -51,8 +80,8 @@ export const onRpcRequest: OnRpcRequestHandler = ({ request }) => {
           params: {
             type: 'alert',
             content: panel([
-              heading(`Assets Health Detection`),
-              text(`Hello, ${address}`),
+              heading(`Risk Detect by ${origin}`),
+              text(`Hello, ${accounts[0]}`),
               ...panelArr,
             ]),
           },
